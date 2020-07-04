c76bdf9c829b8324af9148104814204787b7a013
zhpanf <1728245066@qq.com>
2020-07-04 13:56:44 +0800

    手机商品实现

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -51,9 +51,43 @@
           <div class="swiper-button-next" slot="button-next"></div>
         </swiper>
       </div>
-      <div class="ads-box"></div>
-      <div class="banner"></div>
-      <div class="product-box"></div>
+      <div class="ads-box">
+        <a :href="'/#/product/' + item.id" v-for="(item, index) in adsList" :key="index">
+          <img :src="item.img" />
+        </a>
+      </div>
+      <div class="banner">
+        <a href="'/#/product/30'">
+          <img src="/imgs/banner-1.png" />
+        </a>
+      </div>
+    </div>
+    <div class="product-box">
+      <div class="container">
+        <h2>手机</h2>
+        <div class="wrapper">
+          <div class="banner-left">
+            <a href="/#/product/35">
+              <img src="/imgs/mix-alpha.jpg" alt />
+            </a>
+          </div>
+          <div class="list-box">
+            <div class="list" v-for="(arr, i) in phoneList" :key="i">
+              <div class="item" v-for="(item, j) in arr" :key="j">
+                <span :class="{'new-pro': j % 2 === 0}">新品</span>
+                <div class="item-img">
+                  <img :src="item.mainImage" alt />
+                </div>
+                <div class="item-info">
+                  <h3>{{item.name}}</h3>
+                  <p>{{item.subtitle}}</p>
+                  <p class="price">{{item.price}}元</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <service-bar></service-bar>
   </div>
@@ -138,14 +172,54 @@ export default {
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0]
+      ],
+      adsList: [
+        {
+          id: 33,
+          img: "/imgs/ads/ads-1.png"
+        },
+        {
+          id: 48,
+          img: "/imgs/ads/ads-2.jpg"
+        },
+        {
+          id: 45,
+          img: "/imgs/ads/ads-3.png"
+        },
+        {
+          id: 47,
+          img: "/imgs/ads/ads-4.jpg"
+        }
+      ],
+      phoneList: [
+        [1, 1, 1, 1],
+        [1, 1, 1, 1]
       ]
     };
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      this.axios
+        .get("/products", {
+          params: {
+            categoryId: 100012,
+            pageSize: 8
+          }
+        })
+        .then(res => {
+          this.phoneList = [res.list.slice(0, 4), res.list.slice(4, 8)];
+        });
+    }
   }
 };
 </script>
 <style lang="scss">
 @import "../assets/scss/config.scss";
 @import "../assets/scss/mixin.scss";
+@import "../assets/scss/base.scss";
 .index {
   .swiper-box {
     .nav-menu {
@@ -223,6 +297,102 @@ export default {
       img {
         width: 100%;
         height: 100%；;
+      }
+    }
+  }
+  .ads-box {
+    @include flex();
+    margin-top: 14px;
+    margin-bottom: 31px;
+    a {
+      width: 296px;
+      height: 167px;
+    }
+  }
+  .banner {
+    margin-bottom: 50px;
+  }
+  .product-box {
+    background-color: $colorJ;
+    padding: 30px 0 50px;
+    h2 {
+      font-size: $fontF;
+      height: 21px;
+      line-height: 21px;
+      color: $colorB;
+      margin-bottom: 20px;
+    }
+    .wrapper {
+      display: flex;
+      .banner-left {
+        margin-right: 16px;
+        img {
+          width: 224px;
+          height: 619px;
+        }
+      }
+      .list-box {
+        .list {
+          @include flex();
+          width: 986px;
+          margin-bottom: 14px;
+          &:last-child {
+            margin-bottom: 0;
+          }
+          .item {
+            width: 236px;
+            height: 302px;
+            background-color: $colorG;
+            text-align: center;
+            span {
+              display: inline-block; /* span本身是一个行内元素(没有宽度和高度，其本身是被字体撑开的)，不设inline-block的话，它的背景颜色会设置不上去 */
+              width: 67px;
+              height: 24px;
+              font-size: 14px;
+              line-height: 24px;
+              color: $colorG;
+              &.new-pro {
+                /* "新品"时的背景颜色 */
+                background-color: #7ecf68;
+              }
+              &.kill-pro {
+                /* "秒杀"时的背景颜色 */
+                background-color: #e82626;
+              }
+            }
+            .item-img {
+              img {
+                width: 100%;
+                height: 195px;
+              }
+            }
+            .item-info {
+              h3 {
+                font-size: $fontJ;
+                color: $colorB;
+                line-height: 14px;
+                font-weight: bold;
+              }
+              p {
+                color: $colorD;
+                line-height: 13px;
+                margin: 6px auto 13px;
+              }
+              .price {
+                color: #f20a0a;
+                font-size: $fontJ;
+                font-weight: bold;
+                cursor: pointer; /* 设定鼠标的形状为一只伸出食指的手 */
+                &:after {
+                  @include bgImg(22px, 22px, "/imgs/icon-cart-hover.png");
+                  content: " "; /* 使用content为空来占下位 */
+                  margin-left: 5px;
+                  vertical-align: middle;
+                }
+              }
+            }
+          }
+        }
       }
     }
   }
